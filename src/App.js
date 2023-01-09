c65494f5ceb16826ceaf66f92e1cd49c2b98bbf5
Matt <matt@multicode.co.uk>
2023-01-09 14:39:24 +0000

    context

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddTransaction from './components/AddTransaction/AddTransaction';
+import Login from './components/Login';
 import Menu from './components/Menu';
 import FindTransactionsPage from './components/Transactions/FindTransactionsPage';
+import { UserContext } from './contexts/UserContext';
 
 function App() {
 
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentUser, setCurrentUser] = useState({ name : "", role : ""});
 
   return (
     <BrowserRouter>
+    <UserContext.Provider value={{user:currentUser, setUser:setCurrentUser }}>
       <Menu />
       <Routes>
+        <Route path="/login" element = {<Login />} />
         <Route path="/add" element = {<AddTransaction />} />
         <Route path="/find" element = {
           <FindTransactionsPage searchTerm={searchTerm} setSearchTerm={setSearchTerm} /> }
@@ -22,7 +27,8 @@ function App() {
         />
         <Route path="/" element = { <h1>Welcome to the Payments system</h1>}/>
         <Route path="*" element = { <><h1>Sorry - that page doesn't exist</h1><p>Page not found</p></>}/>
-      </Routes>     
+      </Routes>  
+      </UserContext.Provider>   
     </BrowserRouter>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,25 @@
+import { useContext } from 'react';
 import {Link, NavLink} from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
 
 const Menu = () => {
+
+    const currentUser = useContext(UserContext);
+
+    const logout = () => {
+        currentUser.setUser({name:"", role:""});
+    }
+
     return (
     <div className="pageHeader" >
         <h1><Link to="/">Payments Application</Link></h1>
         <ul className="nav">
             <li><Link to="/find">Find a transaction</Link></li>
             <li><NavLink to="/add">New transaction</NavLink></li>
+            {currentUser.user.name === "" && <li><Link to="/login">Log in</Link></li>}
+            {currentUser.user.name !== "" && <li><button onClick={logout} >Log out</button></li>}
         </ul>
+        {currentUser.user.name !== "" && <p>Current user : {currentUser.user.name}</p>}
     </div>);
 }
 
